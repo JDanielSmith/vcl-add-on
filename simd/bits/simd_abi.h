d80e8611fdc06fdf8a0d9b4be56dec2a551944f8
Dan Smith <dan.smith@maxar.com>
2024-01-31 13:05:12 -0500

    Vector_class -> vcl_native_abi

--- simd/bits/simd_abi.h
+++ simd/bits/simd_abi.h
@@ -56,7 +56,7 @@ namespace simd
 		template<size_type N, size_type total_bits> struct fixed_size_;
 		template<size_type N, size_type total_bits_> struct fixed_size_base
 		{
-			static constexpr auto elements_per_vector = N; // aka "width" or "elements per vector"
+			static constexpr auto elements_per_vector = N; // aka "width"
 			static constexpr auto total_bits = total_bits_;
 		};
 		#define VECTORCLASS_fixed_size_(N_, bits_) template<> struct fixed_size_<N_, bits_> : public fixed_size_base<N_, bits_> { }
@@ -69,13 +69,13 @@ namespace simd
 		#undef VECTORCLASS_fixed_size_
 
 		// See tables 2.1 and 2.2 of https://github.com/vectorclass/manual/raw/master/vcl_manual.pdf
-		template<size_type N, typename T> struct Vector_class_;
+		template<size_type N, typename T> struct Vcl_native_abi;
 		template<size_type N, typename T, typename Vec> struct Vector_class_base
 		{
-			using Vector_class = Vec;
-			static constexpr auto elements_per_vector = N; // aka "width" or "elements per vector"
+			using Vector_class = Vec; // e.g., Vec4i
+			static constexpr auto elements_per_vector = N; // aka "width"
 			using type = T; // e.g., `int32_t` or `float`
-			static constexpr auto total_bits = elements_per_vector * sizeof(type) * 8;
+			static constexpr auto total_bits = elements_per_vector * sizeof(type) * 8; // 128, 256 or 512
 
 			static_assert(std::is_same_v<type, Vec_value_type<Vector_class::elementtype()>>);
 			static_assert(elements_per_vector == Vector_class::size());
@@ -83,36 +83,36 @@ namespace simd
 
 			using fixed_size = fixed_size_<elements_per_vector, total_bits>;
 		};
-		#define VECTORCLASS_Vector_class_(N_, type_, suffix_) \
-			template<> struct Vector_class_<N_, type_> : public Vector_class_base<N_, type_, Vec ## N_ ## suffix_> { }
-		#define VECTORCLASS_Vector_class_integer(N_, bits_, suffix_) \
-			VECTORCLASS_Vector_class_(N_, int ## bits_ ## _t, suffix_); VECTORCLASS_Vector_class_(N_, uint ## bits_ ## _t, u ## suffix_)
+		#define VECTORCLASS_Vcl_native_abi(N_, type_, suffix_) \
+			template<> struct Vcl_native_abi<N_, type_> : public Vector_class_base<N_, type_, Vec ## N_ ## suffix_> { }
+		#define VECTORCLASS_Vcl_native_abi_integer(N_, bits_, suffix_) \
+			VECTORCLASS_Vcl_native_abi(N_, int ## bits_ ## _t, suffix_); VECTORCLASS_Vcl_native_abi(N_, uint ## bits_ ## _t, u ## suffix_)
 		// 128 Total bits
-		VECTORCLASS_Vector_class_integer(16, 8, c); // i.e., Vec16c and Vec16uc
-		VECTORCLASS_Vector_class_integer(8, 16, s); // i.e., Vec8s and Vec8us
-		VECTORCLASS_Vector_class_integer(4, 32, i); // i.e., Vec4i and Vec4ui
-		VECTORCLASS_Vector_class_integer(2, 64, q); // i.e., Vec2q and Vec2uq
-		VECTORCLASS_Vector_class_(4, float, f); // i.e., Vec4f
-		VECTORCLASS_Vector_class_(2, double, d); // i.e., Vec2d
+		VECTORCLASS_Vcl_native_abi_integer(16, 8, c); // i.e., Vec16c and Vec16uc
+		VECTORCLASS_Vcl_native_abi_integer(8, 16, s); // i.e., Vec8s and Vec8us
+		VECTORCLASS_Vcl_native_abi_integer(4, 32, i); // i.e., Vec4i and Vec4ui
+		VECTORCLASS_Vcl_native_abi_integer(2, 64, q); // i.e., Vec2q and Vec2uq
+		VECTORCLASS_Vcl_native_abi(4, float, f); // i.e., Vec4f
+		VECTORCLASS_Vcl_native_abi(2, double, d); // i.e., Vec2d
 		// 256 Total bits
-		VECTORCLASS_Vector_class_integer(32, 8, c); // i.e., Vec32c and Vec32uc
-		VECTORCLASS_Vector_class_integer(16, 16, s); // i.e., Vec16s and Vec16us
-		VECTORCLASS_Vector_class_integer(8, 32, i); // i.e., Vec8i and Vec8ui
-		VECTORCLASS_Vector_class_integer(4, 64, q); // i.e., Vec4q and Vec4uq
-		VECTORCLASS_Vector_class_(8, float, f); // i.e., Vec8f
-		VECTORCLASS_Vector_class_(4, double, d); // i.e., Vec4d
+		VECTORCLASS_Vcl_native_abi_integer(32, 8, c); // i.e., Vec32c and Vec32uc
+		VECTORCLASS_Vcl_native_abi_integer(16, 16, s); // i.e., Vec16s and Vec16us
+		VECTORCLASS_Vcl_native_abi_integer(8, 32, i); // i.e., Vec8i and Vec8ui
+		VECTORCLASS_Vcl_native_abi_integer(4, 64, q); // i.e., Vec4q and Vec4uq
+		VECTORCLASS_Vcl_native_abi(8, float, f); // i.e., Vec8f
+		VECTORCLASS_Vcl_native_abi(4, double, d); // i.e., Vec4d
 		// 512 Total bits
-		VECTORCLASS_Vector_class_integer(64, 8, c); // i.e., Vec64c and Vec64uc
-		VECTORCLASS_Vector_class_integer(32, 16, s); // i.e., Vec32s and Vec32us
-		VECTORCLASS_Vector_class_integer(16, 32, i); // i.e., Vec16i and Vec16ui
-		VECTORCLASS_Vector_class_integer(8, 64, q); // i.e., Vec8q and Vec8uq
-		VECTORCLASS_Vector_class_(16, float, f); // i.e., Vec16f
-		VECTORCLASS_Vector_class_(8, double, d); // i.e., Vec8d
-		#undef VECTORCLASS_Vector_class_integer
-		#undef VECTORCLASS_Vector_class_
+		VECTORCLASS_Vcl_native_abi_integer(64, 8, c); // i.e., Vec64c and Vec64uc
+		VECTORCLASS_Vcl_native_abi_integer(32, 16, s); // i.e., Vec32s and Vec32us
+		VECTORCLASS_Vcl_native_abi_integer(16, 32, i); // i.e., Vec16i and Vec16ui
+		VECTORCLASS_Vcl_native_abi_integer(8, 64, q); // i.e., Vec8q and Vec8uq
+		VECTORCLASS_Vcl_native_abi(16, float, f); // i.e., Vec16f
+		VECTORCLASS_Vcl_native_abi(8, double, d); // i.e., Vec8d
+		#undef VECTORCLASS_Vcl_native_abi_integer
+		#undef VECTORCLASS_Vcl_native_abi
 
 		template<size_type N, typename T>
-		using Vec = Vector_class_<N, T>::Vector_class;
+		using Vec = Vcl_native_abi<N, T>::Vector_class;
 
 		// See tables 2.3 of https://github.com/vectorclass/manual/raw/master/vcl_manual.pdf
 		template<typename Vec> struct for_use_with_;
@@ -150,22 +150,12 @@ namespace simd
 			using fixed_size = fixed_size_<N, detected_vector_size>;
 
 			template <typename T, size_type total_bits>
-			struct native_abi_ : public Vector_class_<(total_bits / 8) / sizeof(T), T>
-			{
-				using base_type = Vector_class_<(total_bits / 8) / sizeof(T), T>;
-				static_assert(total_bits == base_type::total_bits);
-			};
+			using native_abi_ = Vcl_native_abi<(total_bits / 8) / sizeof(T), T>;
 			template <typename T>
 			using native_abi = native_abi_<T, detected_vector_size>::fixed_size;
 
-			template <typename T, size_type N>
-			struct deduce_t_ : public Vector_class_<N, T>
-			{
-				using base_type = Vector_class_<N, T>;
-				static_assert(N == base_type::elements_per_vector);
-			};
 			template<typename T, size_type N, typename Abi = native_abi_<T, detected_vector_size>>
-			using deduce_t = deduce_t_<T, N>::fixed_size;
+			using deduce_t = Vcl_native_abi<N, T>::fixed_size;
 		}
 	}
 }
