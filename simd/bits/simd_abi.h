2b64ee449d3528a0d4cdd3b7220a4f2ceeda93cf
Dan Smith <dan.smith@maxar.com>
2024-01-31 11:21:05 -0500

    vector_class_

--- simd/bits/simd_abi.h
+++ simd/bits/simd_abi.h
@@ -1,5 +1,7 @@
 #pragma once
 
+#include <type_traits>
+
 #include "simd.h"
 
 #if _MSC_VER
@@ -41,88 +43,12 @@ namespace simd
 		}
 		static constexpr auto detected_vector_size = detect_vector_size();
 
-		// See tables 2.1 and 2.2 of https://github.com/vectorclass/manual/raw/master/vcl_manual.pdf
-		template<size_type N, typename T> struct Vec_N_T;
-		// 128 Total bits
-		template<> struct Vec_N_T<16, int8_t> { using type = Vec16c; };
-		template<> struct Vec_N_T<16, uint8_t> { using type = Vec16uc; };
-		template<> struct Vec_N_T<8, int16_t> { using type = Vec8s; };
-		template<> struct Vec_N_T<8, uint16_t> { using type = Vec8us; };
-		template<> struct Vec_N_T<4, int32_t> { using type = Vec4i; };
-		template<> struct Vec_N_T<4, uint32_t> { using type = Vec4ui; };
-		template<> struct Vec_N_T<2, int64_t> { using type = Vec2q; };
-		template<> struct Vec_N_T<2, uint64_t> { using type = Vec2uq; };
-		template<> struct Vec_N_T<4, float> { using type = Vec4f; };
-		template<> struct Vec_N_T<2, double> { using type = Vec2d; };
-		// 256 Total bits
-		template<> struct Vec_N_T<32, int8_t> { using type = Vec32c; };
-		template<> struct Vec_N_T<32, uint8_t> { using type = Vec32uc; };
-		template<> struct Vec_N_T<16, int16_t> { using type = Vec16s; };
-		template<> struct Vec_N_T<16, uint16_t> { using type = Vec16us; };
-		template<> struct Vec_N_T<8, int32_t> { using type = Vec8i; };
-		template<> struct Vec_N_T<8, uint32_t> { using type = Vec8ui; };
-		template<> struct Vec_N_T<4, int64_t> { using type = Vec4q; };
-		template<> struct Vec_N_T<4, uint64_t> { using type = Vec4uq; };
-		template<> struct Vec_N_T<8, float> { using type = Vec8f; };
-		template<> struct Vec_N_T<4, double> { using type = Vec4d; };
-		// 512 Total bits
-		template<> struct Vec_N_T<64, int8_t> { using type = Vec64c; };
-		template<> struct Vec_N_T<64, uint8_t> { using type = Vec64uc; };
-		template<> struct Vec_N_T<32, int16_t> { using type = Vec32s; };
-		template<> struct Vec_N_T<32, uint16_t> { using type = Vec32us; };
-		template<> struct Vec_N_T<16, int32_t> { using type = Vec16i; };
-		template<> struct Vec_N_T<16, uint32_t> { using type = Vec16ui; };
-		template<> struct Vec_N_T<8, int64_t> { using type = Vec8q; };
-		template<> struct Vec_N_T<8, uint64_t> { using type = Vec8uq; };
-		template<> struct Vec_N_T<16, float> { using type = Vec16f; };
-		template<> struct Vec_N_T<8, double> { using type = Vec8d; };
-		template<size_type N, typename T>
-		using Vec = Vec_N_T<N, T>::type;
-
-		// See tables 2.3 of https://github.com/vectorclass/manual/raw/master/vcl_manual.pdf
-		template<typename Vec> struct for_use_with_;
-		// 128 Total bits
-		template<> struct for_use_with_<Vec16c> { using type = Vec16cb; };
-		template<> struct for_use_with_<Vec16uc> { using type = Vec16cb; };
-		template<> struct for_use_with_<Vec8s> { using type = Vec8sb; };
-		template<> struct for_use_with_<Vec8us> { using type = Vec8sb; };
-		template<> struct for_use_with_<Vec4i> { using type = Vec4ib; };
-		template<> struct for_use_with_<Vec4ui> { using type = Vec4ib; };
-		template<> struct for_use_with_<Vec2q> { using type = Vec2qb; };
-		template<> struct for_use_with_<Vec2uq> { using type = Vec2qb; };
-		template<> struct for_use_with_<Vec4f> { using type = Vec4fb; };
-		template<> struct for_use_with_<Vec2d> { using type = Vec2db; };
-		// 256 Total bits
-		template<> struct for_use_with_<Vec32c> { using type = Vec32cb; };
-		template<> struct for_use_with_<Vec32uc> { using type = Vec32cb; };
-		template<> struct for_use_with_<Vec16s> { using type = Vec16sb; };
-		template<> struct for_use_with_<Vec16us> { using type = Vec16sb; };
-		template<> struct for_use_with_<Vec8i> { using type = Vec8ib; };
-		template<> struct for_use_with_<Vec8ui> { using type = Vec8ib; };
-		template<> struct for_use_with_<Vec4q> { using type = Vec4qb; };
-		template<> struct for_use_with_<Vec4uq> { using type = Vec4qb; };
-		template<> struct for_use_with_<Vec8f> { using type = Vec8fb; };
-		template<> struct for_use_with_<Vec4d> { using type = Vec4db; };
-		// 512 Total bits
-		template<> struct for_use_with_<Vec64c> { using type = Vec64cb; };
-		template<> struct for_use_with_<Vec64uc> { using type = Vec64cb; };
-		template<> struct for_use_with_<Vec32s> { using type = Vec32sb; };
-		template<> struct for_use_with_<Vec32us> { using type = Vec32sb; };
-		template<> struct for_use_with_<Vec16i> { using type = Vec16ib; };
-		template<> struct for_use_with_<Vec16ui> { using type = Vec16ib; };
-		template<> struct for_use_with_<Vec8q> { using type = Vec8qb; };
-		template<> struct for_use_with_<Vec8uq> { using type = Vec8qb; };
-		template<> struct for_use_with_<Vec16f> { using type = Vec16fb; };
-		template<> struct for_use_with_<Vec8d> { using type = Vec8db; };
-		template<typename Vec>
-		using for_use_with = for_use_with_<Vec>::type;
-
-		// See section 2.5 of https://github.com/vectorclass/manual/raw/master/vcl_manual.pdf
+		// See §2.5 of https://github.com/vectorclass/manual/raw/master/vcl_manual.pdf
 		template<int elementtype> struct Vec_value_type_;
 		//template<> struct Vec_value_type_<2> { using type = bool; }; // compact
 		//template<> struct Vec_value_type_<3> { using type = bool; }; // broad
 		template<> struct Vec_value_type_<4> { using type = std::int8_t; };
-		template<> struct Vec_value_type_<5> { using type =  std::uint8_t; };
+		template<> struct Vec_value_type_<5> { using type = std::uint8_t; };
 		template<> struct Vec_value_type_<6> { using type = std::int16_t; };
 		template<> struct Vec_value_type_<7> { using type = std::uint16_t; };
 		template<> struct Vec_value_type_<8> { using type = std::int32_t; };
@@ -134,6 +60,82 @@ namespace simd
 		template<> struct Vec_value_type_<17> { using type = double; };
 		template<int elementtype>
 		using Vec_value_type = Vec_value_type_<elementtype>::type;
+
+		// See tables 2.1 and 2.2 of https://github.com/vectorclass/manual/raw/master/vcl_manual.pdf
+		template<size_type N, typename T> struct vector_class_;
+		template<size_type N, typename T, typename Vec> struct vector_class_base_
+		{
+			using Vector_class = Vec;
+			static constexpr auto elements_per_vector = N; // aka "width" or "elements per vector"
+			using type = T; // e.g., `int32_t` or `float`
+			static constexpr auto total_bits = elements_per_vector * sizeof(type) * 8;
+
+			static_assert(std::is_same_v<type, Vec_value_type<Vector_class::elementtype()>>);
+			static_assert(elements_per_vector == Vector_class::size());
+			static_assert(total_bits <= max_vector_size);
+		};
+
+		#define VECTORCLASS_vector_class_(N_, type_, suffix_) \
+			template<> struct vector_class_<N_, type_> : public vector_class_base_<N_, type_, Vec ## N_ ## suffix_> { }
+		#define VECTORCLASS_vector_class_integer(N_, bits_, suffix_) \
+			VECTORCLASS_vector_class_(N_, int ## bits_ ## _t, suffix_); 	VECTORCLASS_vector_class_(N_, uint ## bits_ ## _t, u ## suffix_)
+
+		// 128 Total bits
+		VECTORCLASS_vector_class_integer(16, 8, c); // i.e., Vec16c and Vec16uc
+		VECTORCLASS_vector_class_integer(8, 16, s); // i.e., Vec8s and Vec8us
+		VECTORCLASS_vector_class_integer(4, 32, i); // i.e., Vec4i and Vec4ui
+		VECTORCLASS_vector_class_integer(2, 64, q); // i.e., Vec2q and Vec2uq
+		VECTORCLASS_vector_class_(4, float, f); // i.e., Vec4f
+		VECTORCLASS_vector_class_(2, double, d); // i.e., Vec2d
+		// 256 Total bits
+		VECTORCLASS_vector_class_integer(32, 8, c); // i.e., Vec32c and Vec32uc
+		VECTORCLASS_vector_class_integer(16, 16, s); // i.e., Vec16s and Vec16us
+		VECTORCLASS_vector_class_integer(8, 32, i); // i.e., Vec8i and Vec8ui
+		VECTORCLASS_vector_class_integer(4, 64, q); // i.e., Vec4q and Vec4uq
+		VECTORCLASS_vector_class_(8, float, f); // i.e., Vec8f
+		VECTORCLASS_vector_class_(4, double, d); // i.e., Vec4d
+		// 512 Total bits
+		VECTORCLASS_vector_class_integer(64, 8, c); // i.e., Vec64c and Vec64uc
+		VECTORCLASS_vector_class_integer(32, 16, s); // i.e., Vec32s and Vec32us
+		VECTORCLASS_vector_class_integer(16, 32, i); // i.e., Vec16i and Vec16ui
+		VECTORCLASS_vector_class_integer(8, 64, q); // i.e., Vec8q and Vec8uq
+		VECTORCLASS_vector_class_(16, float, f); // i.e., Vec16f
+		VECTORCLASS_vector_class_(8, double, d); // i.e., Vec8d
+		#undef VECTORCLASS_vector_class_integer
+		#undef VECTORCLASS_vector_class_
+
+		template<size_type N, typename T>
+		using Vec = vector_class_<N, T>::Vector_class;
+
+		// See tables 2.3 of https://github.com/vectorclass/manual/raw/master/vcl_manual.pdf
+		template<typename Vec> struct for_use_with_;
+		#define VECTORCLASS_for_use_with_(N_, suffix_) \
+			template<> struct for_use_with_<Vec ## N_ ## suffix_> { using Vector_class = Vec ## N_ ## suffix_ ## b; }; \
+			template<> struct for_use_with_<Vec ## N_ ## u ## suffix_> { using Vector_class = Vec ## N_ ## suffix_ ## b; }
+		// 128 Total bits
+		VECTORCLASS_for_use_with_(16, c); // i.e., Vec16cb
+		VECTORCLASS_for_use_with_(8, s); // i.e., Vec8sb
+		VECTORCLASS_for_use_with_(4, i); // i.e., Vec4ib
+		VECTORCLASS_for_use_with_(2, q); // i.e., Vec2qb
+		template<> struct for_use_with_<Vec4f> { using Vector_class = Vec4fb; };
+		template<> struct for_use_with_<Vec2d> { using Vector_class = Vec2db; };
+		// 256 Total bits
+		VECTORCLASS_for_use_with_(32, c); // i.e., Vec32cb
+		VECTORCLASS_for_use_with_(16, s); // i.e., Vec16sb
+		VECTORCLASS_for_use_with_(8, i); // i.e., Vec8ib
+		VECTORCLASS_for_use_with_(4, q); // i.e., Vec4qb
+		template<> struct for_use_with_<Vec8f> { using Vector_class = Vec8fb; };
+		template<> struct for_use_with_<Vec4d> { using Vector_class = Vec4db; };
+		// 512 Total bits
+		VECTORCLASS_for_use_with_(64, c); // i.e., Vec64cb
+		VECTORCLASS_for_use_with_(32, s); // i.e., Vec32sb
+		VECTORCLASS_for_use_with_(16, i); // i.e., Vec16ib
+		VECTORCLASS_for_use_with_(8, q); // i.e., Vec8qb
+		template<> struct for_use_with_<Vec16f> { using Vector_class = Vec16fb; };
+		template<> struct for_use_with_<Vec8d> { using Vector_class = Vec8db; };
+		#undef VECTORCLASS_for_use_with_
+		template<typename Vec>
+		using for_use_with = for_use_with_<Vec>::Vector_class;
 
 		template<size_type width> struct fixed_size_;
 		template<> struct fixed_size_<2> { static constexpr size_type vector_size = 128; };
