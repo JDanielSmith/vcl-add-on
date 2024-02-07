72068913ce426f07228cccfa58edf26e0f866865
Dan Smith <dan.smith@maxar.com>
2024-02-07 12:39:40 -0500

    all_of()

--- simd/simd_alg.h
+++ simd/simd_alg.h
@@ -37,6 +37,13 @@ namespace simd
 		using Vec_b = basic_simd_mask<Bytes, Abi>::Vec;
 		return horizontal_or(static_cast<Vec_b>(m));
 	}
+
+	template<size_t Bytes, typename Abi>
+	constexpr bool all_of(const basic_simd_mask<Bytes, Abi>& m) noexcept
+	{
+		using Vec_b = basic_simd_mask<Bytes, Abi>::Vec;
+		return horizontal_and(static_cast<Vec_b>(m));
+	}
 }
 
 #ifdef VCL_NAMESPACE
